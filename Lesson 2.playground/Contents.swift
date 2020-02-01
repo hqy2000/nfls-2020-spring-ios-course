d2d95074e4ce0ad328720404547993129ce88493
Qingyang Hu <me@hqy.moe>
2020-01-31 22:05:52 -0800

    Initialize it to SwiftUI

--- Lesson 2.playground/Contents.swift
+++ Lesson 2.playground/Contents.swift
@@ -1,21 +1,10 @@
-//: A UIKit based Playground for presenting user interface
-  
-import UIKit
+import SwiftUI
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
-        
-        view.addSubview(label)
-        self.view = view
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
     }
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+PlaygroundPage.current.setLiveView(ContentView())
