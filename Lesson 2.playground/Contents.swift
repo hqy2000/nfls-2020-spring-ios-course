5fd77d96c833e3589e7b043cad01bb6209e55513
Qingyang Hu <me@hqy.moe>
2020-01-31 22:45:31 -0800

    Basic views

--- Lesson 2.playground/Contents.swift
+++ Lesson 2.playground/Contents.swift
@@ -3,7 +3,34 @@ import PlaygroundSupport
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        CardView()
+    }
+}
+
+struct CardView: View {
+    @State private var showingDefinition = true
+    var body: some View {
+        VStack {
+            if showingDefinition {
+                WordView()
+                    .onTapGesture { self.showingDefinition.toggle()}
+            } else {
+                DefinitionView()
+                   .onTapGesture { self.showingDefinition.toggle()}
+            }
+        }
+    }
+}
+
+struct WordView: View {
+    var body: some View {
+        Text("寝る")
+    }
+}
+
+struct DefinitionView: View {
+    var body: some View {
+        Text("to sleep")
     }
 }
 
